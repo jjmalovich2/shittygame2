1e69f459a5c55f4b183c367784bfcacc65e15d56
jjmalovich2 <26jmalovich@salpointe.org>
2025-03-24 14:48:38 -0700

    Create gun.js

---
+++ game/src/gun.js
@@ -0,0 +1,13 @@
+export const GUNS = {
+    GLOCK18: {
+        damage: 20,
+        rpm: 1200,
+        rps: 20,
+        recoil: 8,
+        mag: {
+            normal: 23,
+            extended: 33,
+            drum: 50,
+        }
+    }
+}
